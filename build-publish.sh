21ae9aaa237b604914f3cbd94f19c2352005faf4
Chris Arter <chris@arter.dev>
2025-01-16 17:54:37 -0500

    remove script

--- build-publish.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-docker login ghcr.io -u dynamik-dev --password-stdin <<< "$GITHUB_TOKEN"
-docker compose -f docker-compose.build.yml build
-docker compose -f docker-compose.build.yml push
